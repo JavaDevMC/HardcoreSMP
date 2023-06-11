3c61398fe069309c50cd9c79a0f2774955527a2f
Phil <Wolfgangreichelt008@gmail.com>
2023-06-11 13:23:48 +0200

    awdwad

--- src/main/java/de/einphil/hardcoresmp/GriefingLogger.java
+++ src/main/java/de/einphil/hardcoresmp/GriefingLogger.java
@@ -12,6 +12,8 @@ import java.util.Date;
 public class GriefingLogger {
     private static final String LOG_DIRECTORY_PATH = "plugins/HardcoreSMP/";
 
+
+
     private static final String LOG_FILE_PREFIX = "grieflog";
     private static final String LOG_FILE_EXTENSION = ".txt";
 
